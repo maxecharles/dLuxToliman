aad9ccdfbc5f8cad7cd190b44806fac0b4020a61
maxecharles <max.charles@sydney.edu.au>
2023-10-20 16:29:50 +1100

    written up detector_layers

--- dLuxToliman/__init__.py
+++ dLuxToliman/__init__.py
@@ -3,14 +3,15 @@ __version__ = "0.2.0"
 
 # Import as modules
 from . import optical_systems
-from .layers import optical_layers
+from . import layers
 from . import gradient_energy
 from . import instruments
 from . import sources
 
 # Wavefronts and Optics
 from .optical_systems import *
-from dLuxToliman.layers.optical_layers import *
+from .layers.optical_layers import *
+from .layers.detector_layers import *
 from .gradient_energy import *
 from .instruments import *
 from .sources import *
@@ -18,7 +19,7 @@ from .sources import *
 # Add to __all__
 modules = [
     optical_systems,
-    optical_layers,
+    layers,
     gradient_energy,
     instruments,
     sources,
